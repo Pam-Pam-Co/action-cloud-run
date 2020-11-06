e81eb8b2fc40ed62ddf72a29e5340228ce4f5b18
Emre Turan <turan.emre@gmail.com>
2020-11-06 18:33:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ gcloud auth configure-docker
 docker build -t "$INPUT_IMAGE" .
 docker push "$INPUT_IMAGE"
 
-gcloud run deploy "$INPUT_SERVICE" \
+gcloud beta run deploy "$INPUT_SERVICE" \
   --image "$INPUT_IMAGE" \
   --region "$INPUT_REGION" \
   --platform managed \
